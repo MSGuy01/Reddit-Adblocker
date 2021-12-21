1aa24f97e224dd7842765060707c3f7756c2185b
MSGuy01 <msguy01@outlook.com>
2021-12-21 14:18:45 -0800

    Updated stuff

--- src/content.js
+++ src/content.js
@@ -18,29 +18,29 @@ chrome.runtime.sendMessage({greeting: window.location.href}, function(response) 
 });
 if (window.location.href.substr(0, 22) == "https://www.reddit.com" || window.location.href.substr(0, 19) == "https://reddit.com") {
     var checker = window.setInterval(function() {
+        console.log('***********************hi2');
         for (let i = 0; i < document.getElementsByTagName("div").length; i++) {
             for (let j = 0; j < document.getElementsByTagName("div")[i].className.length; j++) {
-                if (document.getElementsByTagName("div")[i].className.substr(j, 8) == "promoted") {
+                if (document.getElementsByTagName("div")[i].className.substr(j, 12) == "promotedLink") {
                     console.log('delete promoted');
                     document.getElementsByTagName("div")[i].parentNode.removeChild(document.getElementsByTagName("div")[i]);
+                }
+                else {
+                    console.log(document.getElementsByTagName("div")[i].className);
                 }
                 if (document.getElementsByTagName("div")[i].className.substr(j, 4) == "Post" || document.getElementsByTagName("div")[i].className.substr(j, 7) == "Comment") {
                     for (let k = 0; k < document.getElementsByTagName("div")[i].getElementsByTagName("a").length; k++) {
                         for (let l = 0; l < mods.length; l++) {
-                            //TODO: MAKE SURE ALL MODS WORK
                             if (document.getElementsByTagName("div")[i].getElementsByTagName("a")[k].href == "https://www.reddit.com/user/" + mods[l] + "/") {
                                 console.log('delete power mod post');
                                 document.getElementsByTagName("div")[i].parentNode.removeChild(document.getElementsByTagName("div")[i]);
-                            }
-                            else {
-                                console.log(document.getElementsByTagName("div")[i].getElementsByTagName("a")[k].href);
                             }
                         }
                     }
                 }
                 if (document.getElementsByTagName("div")[i].className.substr(j, 12) == "scrollerItem") {
                     for (let k = 0; k < document.getElementsByTagName("div")[i].getElementsByTagName("a").length; k++) {
-                        if (document.getElementsByTagName("div")[i].getElementsByTagName("a")[k].href.substr(0, 27) == "https://www.reddit.com/rpan") {
+                        if (document.getElementsByTagName("div")[i].getElementsByTagName("a")[k].href.substr(0, 5) == "/rpan") {
                             console.log('delete live');
                             document.getElementsByTagName("div")[i].parentNode.removeChild(document.getElementsByTagName("div")[i]);
                         }
